53541fefda5f67eb0a6a235642ee8d81a8213879
Ahmed-M-shedid <125396275+Ahmed-M-shedid@users.noreply.github.com>
2024-03-17 15:14:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function calculatePrice() {
+    var pages = parseInt(document.getElementById('pages').value);
+    var covers = parseInt(document.getElementById('covers').value);
+    var profit;
+    var price;
+    var anerror = "Error: Too many covers";
+
+    if (covers == 1) {
+        profit = 100;
+    } else if (covers >= 2 && covers <= 4) {
+        profit = 50 * (covers - 1) + 100;
+    } else if (covers >= 5) {
+        profit = 250 + 25 * (covers - 4);
+    } else {
+        document.getElementById('result').innerText = anerror;
+        return;
+    }
+
+    price = covers * 10 + Math.floor(pages * 0.5 * 0.65) + profit;
+
+    document.getElementById('result').innerText = "The Book Price is " + price;
+}
+
+function calculateProfit() {
+    var covers = parseInt(document.getElementById('covers').value);
+    var profit;
+    var anerror = "Error: Too many covers";
+
+    if (covers == 1) {
+        profit = 100;
+    } else if (covers >= 2 && covers <= 4) {
+        profit = 50 * (covers - 1) + 100;
+    } else if (covers >= 5) {
+        profit = 250 + 25 * (covers - 4);
+    } else {
+        document.getElementById('result').innerText = anerror;
+        return;
+    }
+
+    document.getElementById('result').innerText = "The Profit is " + profit;
+}
